620b55787ce03f4dbbb01682928c55d6074684d0
JiWoong Lee <jiwoong.lee@emotion.co.kr>
2018-01-22 12:00:59 +0900

    test

--- s3FileWork/src/main/java/com/s3FileWork/S3FileWorkApplication.java
+++ s3FileWork/src/main/java/com/s3FileWork/S3FileWorkApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S3FileWorkApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { s
 		SpringApplication.run(S3FileWorkApplication.class, args);
 	}
 }
